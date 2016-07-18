8ff6d592a430da13325659bd4191fefd85766528
mtohare16 <mtohare16@gmail.com>
2016-07-17 21:56:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var cardOne = "queen";
+
+var cardTwo = "queen";
+
+var cardThree = "king";
+
+var cardFour = "king";
+
+
+
+//if (cardTwo === cardFour) {
+//	alert("Sorry, try again");
+//} else if (cardTwo === cardThree) {
+//	alert("Sorry, try again");
+//} else if (cardThree === cardFour) {
+//	alert("You found a match!");
+//} else if (cardOne === cardTwo) {
+//	alert("You found a match!");
+//} else if (cardOne === cardThree){
+//	alert("Sorry, try again");
+//} else if (cardOne === cardFour){
+//	alert("Sorry, try again");
+//}
+
+var gameBoard = document.getElementById('game-board')
